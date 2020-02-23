eca8a09a4d6cadce83b5a2d31d7fe0f89550871d
T.K <kalsangde1@hotmail.com>
2020-02-23 15:42:35 -0500

    Update DBSetup.java

--- ResturantProject/src/com/resturant/DBSetup.java
+++ ResturantProject/src/com/resturant/DBSetup.java
@@ -1,13 +1,8 @@
 package com.resturant;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 public class DBSetup {
 	
 
 	public static String dbUrl = "jdbc:mysql://aws-database.c44byqktlmyn.us-east-2.rds.amazonaws.com:3306/Restuarants?useSSL=false";
-	public static Connection conn;
 	public static String dbUsername = "";				// uninitialized variable
 	public static String dbPassword = "";				// uninitialized variable
 	public static String jdbc_driver = "com.mysql.cj.jdbc.Driver";
