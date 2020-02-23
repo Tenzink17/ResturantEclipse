f5859265add3bc853f0342152f515590a24e4094
T.K <kalsangde1@hotmail.com>
2020-02-23 13:05:26 -0500

    Update DBSetup.java

--- ResturantProject/src/com/resturant/DBSetup.java
+++ ResturantProject/src/com/resturant/DBSetup.java
@@ -8,34 +8,10 @@ public class DBSetup {
 
 	public static String dbUrl = "jdbc:mysql://aws-database.c44byqktlmyn.us-east-2.rds.amazonaws.com:3306/Restuarants?useSSL=false";
 	public static Connection conn;
-	public static String dbUsername = "admin";
-	public static String dbPassword = "admin123";
+	public static String dbUsername = "";				// uninitialized variable
+	public static String dbPassword = "";				// uninitialized variable
 	public static String jdbc_driver = "com.mysql.cj.jdbc.Driver";
 	
-	public static Connection getDbConnection()
-	{
-		
-		String dbUrl = "jdbc:mysql://aws-database.c44byqktlmyn.us-east-2.rds.amazonaws.com:3306/Restuarants?useSSL=false";
 	
-		String dbUsername = "admin";
-		String dbPassword = "admin123";
-		
-		return getConnection(dbUrl,dbUsername,dbPassword);
-	}
-	
-	private static Connection getConnection(String dbUrl2, String dbUsername2,
-			String dbPassword2) {
-		// TODO Auto-generated method stub
-		try {
-			Class.forName(jdbc_driver);
-			conn = DriverManager.getConnection(dbUrl2,dbUsername2,dbPassword2);
-		} catch (SQLException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return conn;
-	}
-	
-
 }
 
